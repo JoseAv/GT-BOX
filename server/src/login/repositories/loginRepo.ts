8caf05452726a931ed210f35bf3dbbe6fd0de0ce
JoseAv <jm.aranav@gmail.com>
2025-09-26 09:39:48 -0600

    Agregado la function para validar y funcionando

---
+++ server/src/login/repositories/loginRepo.ts
@@ -0,0 +1,30 @@
+import QueryTypes from "sequelize/lib/query-types"
+import { db } from "../../shared/config/db/sequelize.js"
+import type { ResultDB } from "../../shared/interfaces/DB.js"
+import type { ValidationError } from "../../user/interfaces/user.js"
+
+export class RepoLogin {
+
+    static login = async ({ email }: { email: string }): Promise<ValidationError> => {
+        try {
+
+            const queryLogin: Array<ResultDB> = await db.query('SELECT (loginUser(:user_id)) as result;', { type: QueryTypes.SELECT }
+            )
+            if (!queryLogin || queryLogin.length === 0 || !queryLogin[0])
+                return [400, { message: 'Empty Data to show' }]
+
+            const responseDb = queryLogin[0].result
+            console.log('Information de la DB', responseDb)
+            return [responseDb.http_code, { ...responseDb }]
+
+
+        } catch (error) {
+            return [400, { message: String(error) }]
+
+        }
+    }
+
+
+
+
+}
--- server/src/login/validations/loginValidation.ts
+++ server/src/login/validations/loginValidation.ts
@@ -1,4 +1,5 @@
 import type { loginUser } from "../interfaces/login.js"
+import { RepoLogin } from "../repositories/loginRepo.js"
 import { SchemaLoginUser } from "./loginSchema.js"
 
 
@@ -11,9 +12,15 @@ export const Validationlogin = async ({ user }: { user: loginUser }) => {
             return [400, { message: 'Correo o clave incorrectos' }]
 
         //? 4. el modelo debe de mandar a llamar al repo despues de verificar
+        const [status, loginUser] = await RepoLogin.login({ email: user.email })
 
         //? 5. El repo devuelve si encontro al user
+        if (status >= 400)
+            return [400, { message: 'No se encontro el user' }]
+
+
         //? 6. El modelo va a verificar la contrase;a
+
         //? 7 el modelo me va a regresar la informacion ya con el jwt
         //? 8 depende del status tomamos decicion
 
