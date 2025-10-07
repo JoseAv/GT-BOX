34ca5e952005f1c848d636a774fd845fd343e6fc
JoseAv <jm.aranav@gmail.com>
2025-10-07 15:37:16 -0600

    Terminado el manejo de rutas

--- front/src/shared/auth/hooks/InfoUser.tsx
+++ front/src/shared/auth/hooks/InfoUser.tsx
@@ -1,9 +1,6 @@
 import { useQuery } from "@tanstack/react-query"
 import { CallAuth, PruebaUserCookie } from "../api/CallAuth"
 
-
-
-
 export const InfoUser = () => {
     const query = useQuery({
         queryKey: ['userInfo'],
